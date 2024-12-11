a7c4fd25b7f799f4a5cd5af48dcd6f1be78dfd0d
Sean C. Weeks <112424278+scweeks@users.noreply.github.com>
2024-12-11 16:48:55 -0500

    Updated the SensorFactory to properly match the SensorAC and child class constructors whiich now include the name, ipAddress, and port adtributes for the main factory method and create sensors using only the name for the second.

--- Factories/SensorFactory.h
+++ Factories/SensorFactory.h
@@ -14,22 +14,22 @@ using namespace std;
 class SensorFactory {
 public:
     // Create a new sensor object for the new sensor and set the connection details
-    static std::shared_ptr<SensorIF> MakeSensor(const std::string& type, string ipAddress, int port) {
-        if (type == "Wind") return make_shared<WindSensor>(ipAddress, port);
-        if (type == "Temp") return make_shared<TempSensor>(ipAddress, port);
-        if (type == "Humidity") return make_shared<HumiditySensor>(ipAddress, port);
-        if (type == "Barometric") return make_shared<BarometricSensor>(ipAddress, port);
-        if (type == "Rain") return make_shared<BarometricSensor>(ipAddress, port);
+    static std::shared_ptr<SensorIF> MakeSensor(const std::string& type, string name, string ipAddress, int port) {
+        if (type == "Wind") return make_shared<WindSensor>(name, ipAddress, port);
+        if (type == "Temp") return make_shared<TempSensor>(name, ipAddress, port);
+        if (type == "Humidity") return make_shared<HumiditySensor>(name, ipAddress, port);
+        if (type == "Barometric") return make_shared<BarometricSensor>(name, ipAddress, port);
+        if (type == "Rain") return make_shared<BarometricSensor>(name, ipAddress, port);
         return nullptr;
     }
 
     // Overloaded method to create a sensor with only a name
-    static std::shared_ptr<SensorIF> MakeSensor(const std::string& type) {
-        if (type == "Wind") return make_shared<WindSensor>();
-        if (type == "Temp") return make_shared<TempSensor>();
-        if (type == "Humidity") return make_shared<HumiditySensor>();
-        if (type == "Barometric") return make_shared<BarometricSensor>();
-        if (type == "Rain") return make_shared<BarometricSensor>();
+    static std::shared_ptr<SensorIF> MakeSensor(const std::string& type, string name) {
+        if (type == "Wind") return make_shared<WindSensor>(name);
+        if (type == "Temp") return make_shared<TempSensor>(name);
+        if (type == "Humidity") return make_shared<HumiditySensor>(name);
+        if (type == "Barometric") return make_shared<BarometricSensor>(name);
+        if (type == "Rain") return make_shared<BarometricSensor>(name);
         return nullptr;
     }
 };
